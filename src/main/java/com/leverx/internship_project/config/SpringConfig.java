efaace31c4e7f2a560f448b5fc9757557954c802
funkylop <grommog6@mail.ru>
2022-01-19 14:06:36 +0300

    created and configured basic spring web app

---
+++ src/main/java/com/leverx/internship_project/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.leverx.internship_project.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ComponentScan("com.leverx.internship_project")
+@Configuration
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+}
