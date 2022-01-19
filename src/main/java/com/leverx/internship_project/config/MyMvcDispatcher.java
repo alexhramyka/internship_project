9e70875c59620ba97ee2329c927d3cbc04a774f5
funkylop <grommog6@mail.ru>
2022-01-19 14:06:36 +0300

    created and configured basic spring web app

---
+++ src/main/java/com/leverx/internship_project/config/MyMvcDispatcher.java
@@ -0,0 +1,20 @@
+package com.leverx.internship_project.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyMvcDispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
