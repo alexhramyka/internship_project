192789dacfd41d48e6229353a787a436aa581c5d
funkylop <grommog6@mail.ru>
2022-01-23 20:34:39 +0300

    m0-design & setup-basic app skeleton : Added SpringConfig.java and WebConfig.java

--- src/main/java/com/leverx/internship/project/config/SpringConfig.java
+++ src/main/java/com/leverx/internship/project/config/SpringConfig.java
@@ -1,11 +1,12 @@
-package com.leverx.internship_project.config;
+package com.leverx.internship.project.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ComponentScan("com.leverx.internship_project")
+@ComponentScan("com.leverx.internship.project")
 @Configuration
 @EnableWebMvc
-public class SpringConfig implements WebMvcConfigurer {}
+public class SpringConfig implements WebMvcConfigurer {
+}
