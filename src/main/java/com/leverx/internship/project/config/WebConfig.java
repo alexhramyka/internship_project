ea27051ba7ebea3f853339611ddf34c912042b9a
funkylop <grommog6@mail.ru>
2022-01-24 12:23:01 +0300

    Some changes in WebConfig.java

--- src/main/java/com/leverx/internship/project/config/WebConfig.java
+++ src/main/java/com/leverx/internship/project/config/WebConfig.java
@@ -1,21 +1,22 @@
 package com.leverx.internship.project.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return new Class<?>[] {SpringConfig.class};
-  }
-
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return new Class[]{};
-  }
+public class WebConfig implements WebApplicationInitializer {
 
   @Override
-  protected String[] getServletMappings() {
-    return new String[] {"/"};
+  public void onStartup(ServletContext servletContext) {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(SpringConfig.class);
+
+    ServletRegistration.Dynamic servlet =
+        servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+    servlet.setLoadOnStartup(1);
+    servlet.addMapping("/api/*");
   }
 }
