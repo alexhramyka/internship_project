692ac6ecab1ad63fc9e1f90ec559f2b04d950ead
funkylop <grommog6@mail.ru>
2022-01-31 11:26:11 +0300

    m1 - basic Project functionality - Infrastructure:
    Added Role.java, UserDto.java, UserConverter.java

---
+++ src/main/java/com/leverx/internship/project/user/web/converter/UserConverter.java
@@ -0,0 +1,27 @@
+package com.leverx.internship.project.user.web.converter;
+
+import com.leverx.internship.project.user.repository.entity.User;
+import com.leverx.internship.project.user.web.dto.UserDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConverter {
+  private final ModelMapper mapper = new ModelMapper();
+
+  public User toEntity(UserDto userDto) {
+    mapper.typeMap(UserDto.class, User.class)
+        .addMappings(mapper -> mapper.skip(User::setId));
+    if (userDto != null) {
+      return mapper.map(userDto, User.class);
+    } else return null;
+  }
+
+  public UserDto toUserDto(User user) {
+    if(user != null) {
+      return mapper.map(user, UserDto.class);
+    } else {
+      return null;
+    }
+  }
+}
