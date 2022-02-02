c31051c8e3dda861e3930a16a8d08c01b6554c2e
funkylop <grommog6@mail.ru>
2022-02-02 17:30:16 +0300

    m1 - basic Project functionality - CRUD for the user:
    Added ErrorResponse.java and added new method in GlobalExceptionHandler.

--- src/main/java/com/leverx/internship/project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/leverx/internship/project/exception/GlobalExceptionHandler.java
@@ -1,15 +1,28 @@
 package com.leverx.internship.project.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.persistence.EntityNotFoundException;
+import org.webjars.NotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(value = EntityNotFoundException.class)
-  public String entityNotFound(EntityNotFoundException e) {
-      return e.toString();
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<ErrorResponse> notFoundException(NotFoundException ex) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND,
+        String.format("Not found entity by id '%s'", ex.getMessage()));
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ErrorResponse> constraintException(DataIntegrityViolationException ex) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST,
+        String.format("Error because of constraint '%s'", ex.getMessage()));
+
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 }
