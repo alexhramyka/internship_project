ce6524e1862f16867c346ad0e067a706dd0de3e5
funkylop <grommog6@mail.ru>
2022-02-04 14:07:13 +0300

    m1 - basic Project functionality - CRUD for the project:
    Added it and junit5 tests for project controller and service.

---
+++ src/test/java/com/leverx/internship/project/project/service/impl/ProjectServiceTest.java
@@ -0,0 +1,394 @@
+package com.leverx.internship.project.project.service.impl;
+
+import com.leverx.internship.project.project.repository.ProjectRepository;
+import com.leverx.internship.project.project.repository.entity.Project;
+import com.leverx.internship.project.project.service.ProjectService;
+import com.leverx.internship.project.project.web.converter.ProjectConverter;
+import com.leverx.internship.project.project.web.dto.ProjectDto;
+import com.leverx.internship.project.security.Role;
+import com.leverx.internship.project.user.repository.entity.User;
+import com.leverx.internship.project.user.service.UserService;
+import com.leverx.internship.project.user.web.converter.UserConverter;
+import com.leverx.internship.project.user.web.dto.UserDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProjectServiceTest {
+
+  @Mock private ProjectRepository projectRepository;
+  @Mock private UserService userService;
+  private ProjectService projectService;
+  @Mock private ProjectConverter projectConverter;
+  @Mock private UserConverter userConverter;
+
+  ProjectServiceTest() {}
+
+  @BeforeEach
+  public void initUseCase() {
+    projectService =
+        new ProjectServiceImpl(projectRepository, projectConverter, userService, userConverter) {};
+  }
+
+  @Test
+  void findAllPositiveTest() {
+    ProjectDto projectDto = new ProjectDto();
+    projectDto.setName("name");
+    projectDto.setDescription("description");
+    projectDto.setDateStart(LocalDate.now());
+    projectDto.setDateEnd(LocalDate.now());
+    List<ProjectDto> expected = new ArrayList<>();
+    expected.add(projectDto);
+    Project project = new Project();
+    project.setName("name");
+    project.setDescription("description");
+    project.setDateStart(LocalDate.now());
+    project.setDateEnd(LocalDate.now());
+    project.setCreatedAt(LocalDate.now());
+    project.setUpdatedAt(LocalDate.now());
+    Page<Project> projects = new PageImpl<>(List.of(project));
+    when(projectRepository.findAll(any(Pageable.class))).thenReturn(projects);
+
+    ProjectDto projectDto1 = new ProjectDto();
+    projectDto1.setName("name");
+    projectDto1.setDescription("description");
+    projectDto1.setDateStart(LocalDate.now());
+    projectDto1.setDateEnd(LocalDate.now());
+    when(projectConverter.toProjectDto(any(Project.class))).thenReturn(projectDto);
+
+    List<ProjectDto> actual = projectService.findAll(0, 3);
+
+    assertEquals(expected.size(), actual.size());
+  }
+
+  @Test
+  void saveUserPositiveTest() {
+    ProjectDto expected = new ProjectDto();
+    expected.setName("name");
+    expected.setDescription("description");
+    expected.setDateStart(LocalDate.now());
+    expected.setDateEnd(LocalDate.now());
+
+    ProjectDto projectDto = new ProjectDto();
+    projectDto.setName("name");
+    projectDto.setDescription("description");
+    projectDto.setDateStart(LocalDate.now());
+    projectDto.setDateEnd(LocalDate.now());
+
+    Project project = new Project();
+    project.setName("name");
+    project.setDescription("description");
+    project.setDateStart(LocalDate.now());
+    project.setDateEnd(LocalDate.now());
+    project.setCreatedAt(LocalDate.now());
+    project.setUpdatedAt(LocalDate.now());
+    when(projectConverter.toEntity(projectDto)).thenReturn(project);
+
+    ProjectDto projectDto1 = new ProjectDto();
+    projectDto1.setName("name");
+    projectDto1.setDescription("description");
+    projectDto1.setDateStart(LocalDate.now());
+    projectDto1.setDateEnd(LocalDate.now());
+    when(projectConverter.toProjectDto(any(Project.class))).thenReturn(projectDto1);
+
+    Project project1 = new Project();
+    project1.setName("name");
+    project1.setDescription("description");
+    project1.setDateStart(LocalDate.now());
+    project1.setDateEnd(LocalDate.now());
+    project1.setCreatedAt(LocalDate.now());
+    project1.setUpdatedAt(LocalDate.now());
+    when(projectRepository.save(any(Project.class))).thenReturn(project1);
+
+    final ProjectDto result = projectService.create(projectDto);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void FindByIdPositiveTest() {
+    ProjectDto expected = new ProjectDto();
+    expected.setId(1);
+    expected.setName("name");
+    expected.setDescription("description");
+    expected.setDateStart(LocalDate.now());
+    expected.setDateEnd(LocalDate.now());
+
+    Project project = new Project();
+    project.setId(1);
+    project.setName("name");
+    project.setDescription("description");
+    project.setDateStart(LocalDate.now());
+    project.setDateEnd(LocalDate.now());
+    project.setCreatedAt(LocalDate.now());
+    project.setUpdatedAt(LocalDate.now());
+    Optional<Project> projectOptional = Optional.of(project);
+    when(projectRepository.findById(1)).thenReturn(projectOptional);
+
+    ProjectDto projectDto = new ProjectDto();
+    projectDto.setId(1);
+    projectDto.setName("name");
+    projectDto.setDescription("description");
+    projectDto.setDateStart(LocalDate.now());
+    projectDto.setDateEnd(LocalDate.now());
+    when(projectConverter.toProjectDto(any(Project.class))).thenReturn(projectDto);
+
+    final ProjectDto result = projectService.findById(1);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void updateUserPositiveTest() {
+    ProjectDto expected = new ProjectDto();
+    expected.setId(1);
+    expected.setName("name");
+    expected.setDescription("new description");
+    expected.setDateStart(LocalDate.now());
+    expected.setDateEnd(LocalDate.now());
+
+    ProjectDto projectDtoToUpdate = new ProjectDto();
+    projectDtoToUpdate.setDescription("new description");
+
+    Project project = new Project();
+    project.setId(1);
+    project.setName("name");
+    project.setDescription("description");
+    project.setDateStart(LocalDate.now());
+    project.setDateEnd(LocalDate.now());
+    project.setCreatedAt(LocalDate.now());
+    project.setUpdatedAt(LocalDate.now());
+
+    Optional<Project> projectOptional = Optional.of(project);
+    when(projectRepository.findById(1)).thenReturn(projectOptional);
+
+    Project project1 = new Project();
+    project1.setName("name");
+    project1.setDescription("description");
+    project1.setDateStart(LocalDate.now());
+    project1.setDateEnd(LocalDate.now());
+    project1.setCreatedAt(LocalDate.now());
+    project1.setUpdatedAt(LocalDate.now());
+    when(projectConverter.toEntity(any(ProjectDto.class))).thenReturn(project1);
+
+    ProjectDto projectDto = new ProjectDto();
+    projectDto.setId(1);
+    projectDto.setName("name");
+    projectDto.setDescription("new description");
+    projectDto.setDateStart(LocalDate.now());
+    projectDto.setDateEnd(LocalDate.now());
+    when(projectConverter.toUpdatedProjectDto(eq(projectDtoToUpdate), any(Project.class)))
+        .thenReturn(projectDto);
+    when(projectConverter.toProjectDto(any(Project.class))).thenReturn(projectDto);
+
+    Project project2 = new Project();
+    project2.setName("name");
+    project2.setDescription("new description");
+    project2.setDateStart(LocalDate.now());
+    project2.setDateEnd(LocalDate.now());
+    project2.setCreatedAt(LocalDate.now());
+    project2.setUpdatedAt(LocalDate.now());
+    when(projectRepository.save(any(Project.class))).thenReturn(project2);
+
+    final ProjectDto result = projectService.update(1, projectDtoToUpdate);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void deletePositiveTest() {
+    projectService.delete(1);
+
+    verify(projectRepository).deleteById(1);
+  }
+
+  @Test
+  void testAddUserToProject() {
+    ProjectDto expected = new ProjectDto();
+    expected.setId(1);
+    expected.setName("name");
+    expected.setDescription("description");
+    expected.setDateStart(LocalDate.of(2022, 2, 1));
+    expected.setDateEnd(LocalDate.of(2022, 2, 1));
+    expected.setUsersDto(
+        List.of(new UserDto(1, "email@mail.ru", "password", "Ivan", "Ivanov", Role.ADMIN, true)));
+
+    User user = new User();
+    user.setId(1);
+    user.setFirstName("Ivan");
+    user.setLastName("Ivanov");
+    user.setEmail("email@mail.ru");
+    user.setPassword("password");
+    user.setActive(true);
+    user.setRole(Role.ADMIN);
+    user.setCreatedAt(LocalDate.of(2022, 2, 1));
+    user.setUpdatedAt(LocalDate.of(2022, 2, 1));
+    UserDto userDto =
+        new UserDto(1, "email@mail.ru", "password", "Ivan", "Ivanov", Role.ADMIN, true);
+    when(userConverter.toEntity(
+        userDto))
+        .thenReturn(user);
+    when(userService.findById(1)).thenReturn(userDto);
+
+    User user1 = new User();
+    user1.setId(1);
+    user1.setFirstName("Ivan");
+    user1.setLastName("Ivanov");
+    user1.setEmail("email@mail.ru");
+    user1.setPassword("password");
+    user1.setActive(true);
+    user1.setRole(Role.ADMIN);
+    user1.setCreatedAt(LocalDate.of(2022, 2, 1));
+    user1.setUpdatedAt(LocalDate.of(2022, 2, 1));
+    Optional<Project> projectOptional =
+        Optional.of(
+            new Project(
+                1,
+                "name",
+                "description",
+                LocalDate.of(2022, 2, 1),
+                LocalDate.of(2022, 2, 1),
+                LocalDate.of(2022, 2, 1),
+                LocalDate.of(2022, 2, 1),
+                0,
+                0,
+                new ArrayList<>()));
+    projectOptional.get().addEmployee(user1);
+    when(projectRepository.findById(1)).thenReturn(projectOptional);
+
+    User user2 = new User();
+    user2.setId(1);
+    user2.setFirstName("Ivan");
+    user2.setLastName("Ivanov");
+    user2.setEmail("email@mail.ru");
+    user2.setPassword("password");
+    user2.setActive(true);
+    user2.setRole(Role.ADMIN);
+    user2.setCreatedAt(LocalDate.of(2022, 2, 1));
+    user2.setUpdatedAt(LocalDate.of(2022, 2, 1));
+
+    Project project =
+        new Project(
+            1,
+            "name",
+            "description",
+            LocalDate.of(2022, 2, 1),
+            LocalDate.of(2022, 2, 1),
+            LocalDate.of(2022, 2, 1),
+            LocalDate.of(2022, 2, 1),
+            0,
+            0,
+            new ArrayList<>());
+    project.addEmployee(user2);
+    when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+    ProjectDto projectDto = new ProjectDto();
+    projectDto.setId(1);
+    projectDto.setName("name");
+    projectDto.setDescription("description");
+    projectDto.setDateStart(LocalDate.of(2022, 2, 1));
+    projectDto.setDateEnd(LocalDate.of(2022, 2, 1));
+    List<UserDto> usersDto = new ArrayList<>();
+    usersDto.add(userDto);
+    projectDto.setUsersDto(usersDto);
+    when(projectConverter.toProjectDto(any(Project.class))).thenReturn(projectDto);
+    ProjectDto result = projectService.addUserToProject(1, 1);
+
+    assertEquals(expected, result);
+    verify(projectRepository).save(any(Project.class));
+  }
+
+  @Test
+  void testDeleteUserFromProject() {
+    User user = new User();
+    user.setId(1);
+    user.setFirstName("Ivan");
+    user.setLastName("Ivanov");
+    user.setEmail("email@mail.ru");
+    user.setPassword("password");
+    user.setActive(true);
+    user.setRole(Role.ADMIN);
+    user.setCreatedAt(LocalDate.of(2022, 2, 1));
+    user.setUpdatedAt(LocalDate.of(2022, 2, 1));
+    UserDto userDto =
+        new UserDto(1, "email@mail.ru", "password", "Ivan", "Ivanov", Role.ADMIN, true);
+    when(userConverter.toEntity(
+        userDto))
+        .thenReturn(user);
+
+    when(userService.findById(1)).thenReturn(userDto);
+
+    User user1 = new User();
+    user1.setId(1);
+    user1.setFirstName("Ivan");
+    user1.setLastName("Ivanov");
+    user1.setEmail("email@mail.ru");
+    user1.setPassword("password");
+    user1.setActive(true);
+    user1.setRole(Role.ADMIN);
+    user1.setCreatedAt(LocalDate.of(2022, 2, 1));
+    user1.setUpdatedAt(LocalDate.of(2022, 2, 1));
+    Optional<Project> projectOptional =
+        Optional.of(
+            new Project(
+                1,
+                "name",
+                "description",
+                LocalDate.of(2022, 2, 1),
+                LocalDate.of(2022, 2, 1),
+                LocalDate.of(2022, 2, 1),
+                LocalDate.of(2022, 2, 1),
+                0,
+                0,
+                new ArrayList<>()));
+    projectOptional.get().addEmployee(user1);
+    when(projectRepository.findById(1)).thenReturn(projectOptional);
+
+    User user2 = new User();
+    user2.setId(1);
+    user2.setFirstName("Ivan");
+    user2.setLastName("Ivanov");
+    user2.setEmail("email@mail.ru");
+    user2.setPassword("password");
+    user2.setActive(true);
+    user2.setRole(Role.ADMIN);
+    user2.setCreatedAt(LocalDate.of(2022, 2, 1));
+    user2.setUpdatedAt(LocalDate.of(2022, 2, 1));
+
+    Project project =
+        new Project(
+            1,
+            "name",
+            "description",
+            LocalDate.of(2022, 2, 1),
+            LocalDate.of(2022, 2, 1),
+            LocalDate.of(2022, 2, 1),
+            LocalDate.of(2022, 2, 1),
+            0,
+            0,
+            new ArrayList<>());
+    project.addEmployee(user2);
+    project.addEmployee(user2);
+    when(projectRepository.save(any(Project.class))).thenReturn(project);
+
+    projectService.deleteUserFromProject(1,1);
+
+    verify(projectRepository).save(any(Project.class));
+  }
+
+}
---
+++ src/test/java/com/leverx/internship/project/project/web/controller/ProjectControllerTest.java
@@ -0,0 +1,155 @@
+package com.leverx.internship.project.project.web.controller;
+
+import com.leverx.internship.project.config.H2ConfigTest;
+import com.leverx.internship.project.config.SpringConfig;
+import com.leverx.internship.project.config.WebConfig;
+import com.leverx.internship.project.project.web.dto.ProjectDto;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ContextConfiguration(classes = {SpringConfig.class, WebConfig.class, H2ConfigTest.class})
+@WebAppConfiguration(value = "src/main/java/com/leverx/internship/project")
+@Sql(value = "classpath:data-test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "classpath:delete_data-test.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Scope(value = "test")
+public class ProjectControllerTest {
+
+  private final WebApplicationContext webApplicationContext;
+  private MockMvc mockMvc;
+
+  @Autowired
+  public ProjectControllerTest(WebApplicationContext webApplicationContext) {
+    this.webApplicationContext = webApplicationContext;
+  }
+
+  @BeforeEach
+  void setUp() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+  }
+
+  @Test
+  void getAllProjectsTestPositive() throws Exception {
+    MockHttpServletResponse response = mockMvc
+        .perform(
+            get("/projects")
+                .param("size", "3")
+                .param("page", "0")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn()
+        .getResponse();
+
+    System.out.println(response.getContentAsString());
+
+    assertEquals(HttpStatus.OK.value(), response.getStatus());
+  }
+
+  @Test
+  void findUserByIdTestPositive() throws Exception {
+    MockHttpServletResponse response = mockMvc
+        .perform(
+            get("/users/{id}", 2)
+                .param("size", "3")
+                .param("page", "0")
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn()
+        .getResponse();
+
+    System.out.println(response.getContentAsString());
+
+    assertEquals(HttpStatus.OK.value(), response.getStatus());
+  }
+
+  @Test
+  void createTestPositive() throws Exception {
+    String jsonString =
+        new JSONObject()
+            .put("name", "name1")
+            .put("description", "description")
+            .put("dateStart", "2022-02-04")
+            .put("dateEnd", "2022-02-04")
+            .toString();
+    MockHttpServletResponse response = mockMvc
+        .perform(
+            post("/projects")
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andReturn()
+        .getResponse();
+
+    System.out.println(response.getContentAsString());
+
+    assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+  }
+
+  @Test
+  void updateTestPositive() throws Exception {
+    String jsonString =
+        new JSONObject()
+            .put("description", "new description").toString();
+    final MockHttpServletResponse response = mockMvc.perform(put("/projects/{id}", 2)
+            .content(jsonString).contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
+
+    System.out.println(response.getContentAsString());
+
+    assertEquals(HttpStatus.OK.value(), response.getStatus());
+  }
+
+  @Test
+  void deleteTestPositive() throws Exception {
+    final MockHttpServletResponse response = mockMvc.perform(delete("/projects/{id}", 4)
+            .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
+
+    assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus()) ;
+  }
+
+  @Test
+  void addUserToProjectTestPositive() throws Exception {
+    final MockHttpServletResponse response =
+        mockMvc
+            .perform(
+                post("/projects/{idProjects}/users/{idUsers}", 4, 2)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse();
+
+    System.out.println(response.getContentAsString());
+
+    assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+  }
+
+  @Test
+  void deleteUserFromProjectTestPositive() throws Exception {
+    final MockHttpServletResponse response = mockMvc.perform(delete("/projects/{idProjects}/users/{idUsers}", 4, 3)
+            .accept(MediaType.APPLICATION_JSON))
+        .andReturn().getResponse();
+
+    System.out.println(response.getContentAsString());
+
+    assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
+  }
+}
