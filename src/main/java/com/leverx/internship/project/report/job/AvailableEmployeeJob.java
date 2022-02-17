87b0daf56010ba0c2c331ff5d4c38e010314349b
funkylop <grommog6@mail.ru>
2022-02-17 14:49:05 +0300

    m3 - Job_&_Report_to_collect_statistics - Report_to_collect_statistics:
    Some changes.

--- src/main/java/com/leverx/internship/project/report/job/AvailableEmployeeJob.java
+++ src/main/java/com/leverx/internship/project/report/job/AvailableEmployeeJob.java
@@ -9,17 +9,22 @@ import com.leverx.internship.project.project.repository.ProjectRepository;
 import com.leverx.internship.project.project.repository.entity.Project;
 import com.leverx.internship.project.project.web.converter.ProjectConverter;
 import com.leverx.internship.project.project.web.dto.response.ProjectResponse;
-import com.leverx.internship.project.report.model.ReportType;
 import com.leverx.internship.project.report.excel.generator.ExcelWorkbookGenerator;
-import com.leverx.internship.project.user.repository.UserRepository;
+import com.leverx.internship.project.report.model.ReportType;
 import com.leverx.internship.project.user.web.converter.UserConverter;
 import com.leverx.internship.project.user.web.dto.response.UserResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
-import java.util.*;
-
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,44 +36,48 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @PropertySource("classpath:application.properties")
 public class AvailableEmployeeJob {
+
   private final ProjectRepository projectRepository;
-  private final UserRepository userRepository;
   private final UserConverter userConverter;
   private final ProjectConverter projectConverter;
   private final ReportType reportType = ReportType.AvailableEmployeeReport;
   private final ExcelWorkbookGenerator excelWorkbookGenerator;
   private final DepartmentRepository departmentRepository;
   private final DepartmentConverter departmentConverter;
-  private final static String fileName = "%sAvailable_employee-%s.xlsx";
+  private static final String FILE_NAME = "%sAvailable_employee-%s.xlsx";
+  private static final int LIMIT_OF_PROJECTS = 2;
   @Value("${output.file.path}")
   private String pathForOutput;
+  private final Instant nextMonthDate;
 
   @Autowired
   public AvailableEmployeeJob(
       ProjectRepository projectRepository,
-      UserRepository userRepository,
       UserConverter userConverter,
       ProjectConverter projectConverter,
       ExcelWorkbookGenerator excelWorkbookGenerator,
       DepartmentRepository departmentRepository,
-      DepartmentConverter departmentConverter) {
+      DepartmentConverter departmentConverter,
+      Clock clock) {
     this.projectRepository = projectRepository;
-    this.userRepository = userRepository;
     this.userConverter = userConverter;
     this.projectConverter = projectConverter;
     this.excelWorkbookGenerator = excelWorkbookGenerator;
     this.departmentRepository = departmentRepository;
     this.departmentConverter = departmentConverter;
+    this.nextMonthDate = clock.instant().plus(30, ChronoUnit.DAYS);
   }
 
   @Transactional(readOnly = true)
   @Scheduled(cron = "${cron.expression}")
   public void createAvailableEmployeeReport() {
     File file =
-        new File(String.format(fileName, pathForOutput, LocalDate.now().getMonth().name()));
+        new File(String.format(FILE_NAME, pathForOutput, LocalDate.now().getMonth().name()));
     try (FileOutputStream fos = new FileOutputStream(file)) {
-      Workbook workbook = excelWorkbookGenerator.generateReportWorkbook(reportType, getAvailableEmployee());
+      Workbook workbook = excelWorkbookGenerator.generateReportWorkbook(reportType,
+          getAvailableEmployee());
       workbook.write(fos);
+      workbook.close();
     } catch (IOException e) {
       throw new ReportNotFoundException(e.getMessage());
     }
@@ -78,25 +87,21 @@ public class AvailableEmployeeJob {
   public Map<DepartmentResponse, Map<UserResponse, Set<ProjectResponse>>> getAvailableEmployee() {
     List<Department> departments = departmentRepository.findAll();
     Map<DepartmentResponse, Map<UserResponse, Set<ProjectResponse>>> reportResult = new HashMap<>();
-    LocalDate nextMonth = LocalDate.now().plusDays(30);
-    int limitOfProjects = 3;
     departments.forEach(
         department -> {
-    Map<UserResponse, Set<ProjectResponse>> availableEmployees = new HashMap<>();
-    userRepository
-        .findAll()
-        .forEach(
-            employee -> {
-              Set<Project> projects = new HashSet<>(
-                  projectRepository.findProjectsByEmployeesIdAndDateEndAfter(
-                      employee.getId(), nextMonth));
-              if (projects.size() < limitOfProjects) {
-                availableEmployees.put(
-                    userConverter.toUserResponse(employee),
-                    projectConverter.projectSetToProjectRespSet(projects));
-              }
-            });
-          reportResult.put(departmentConverter.toDepartmentResponse(department), availableEmployees);
+          Map<UserResponse, Set<ProjectResponse>> availableEmployees = new HashMap<>();
+          department.getEmployees()
+              .forEach(employee -> {
+                Set<Project> projects = new HashSet<>(
+                    projectRepository.findProjectsByEmployeesIdAndDateEndAfter(employee.getId(),
+                        nextMonthDate));
+                if (projects.size() < LIMIT_OF_PROJECTS) {
+                  availableEmployees.put(userConverter.toUserResponse(employee),
+                      projectConverter.projectSetToProjectRespSet(projects));
+                }
+              });
+          reportResult.put(departmentConverter.toDepartmentResponse(department),
+              availableEmployees);
         });
     return reportResult;
   }
--- src/main/java/com/leverx/internship/project/report/job/WorkloadJob.java
+++ src/main/java/com/leverx/internship/project/report/job/WorkloadJob.java
@@ -8,20 +8,23 @@ import com.leverx.internship.project.exception.ReportNotFoundException;
 import com.leverx.internship.project.project.repository.ProjectRepository;
 import com.leverx.internship.project.project.web.converter.ProjectConverter;
 import com.leverx.internship.project.project.web.dto.response.ProjectResponse;
-import com.leverx.internship.project.report.model.ReportType;
 import com.leverx.internship.project.report.excel.generator.ExcelWorkbookGenerator;
+import com.leverx.internship.project.report.model.ReportType;
 import com.leverx.internship.project.user.web.converter.UserConverter;
 import com.leverx.internship.project.user.web.dto.response.UserResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
-import java.util.Set;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,9 +44,11 @@ public class WorkloadJob {
   private final DepartmentConverter departmentConverter;
   private final ExcelWorkbookGenerator excelWorkbookGenerator;
   private final ReportType reportType = ReportType.WorkloadReport;
-  private final static String fileName = "%sWorkload_by_department-%s.xlsx";
+  private static final String FILE_NAME = "%sWorkload_by_department-%s.xlsx";
   @Value("${output.file.path}")
   private String pathForOutput;
+  private final Instant dateMonthAgo;
+  private final Instant dateCurrentMonth;
 
   @Autowired
   public WorkloadJob(
@@ -52,23 +57,28 @@ public class WorkloadJob {
       DepartmentRepository departmentRepository,
       ProjectConverter projectConverter,
       DepartmentConverter departmentConverter,
-      ExcelWorkbookGenerator excelWorkbookGenerator) {
+      ExcelWorkbookGenerator excelWorkbookGenerator,
+      Clock clock) {
     this.projectRepository = projectRepository;
     this.userConverter = userConverter;
     this.departmentRepository = departmentRepository;
     this.projectConverter = projectConverter;
     this.departmentConverter = departmentConverter;
     this.excelWorkbookGenerator = excelWorkbookGenerator;
+    this.dateMonthAgo = clock.instant().minus(30, ChronoUnit.DAYS);
+    this.dateCurrentMonth = clock.instant();
   }
 
   @Transactional(readOnly = true)
   @Scheduled(cron = "${cron.expression}")
   public void createWorkloadReport() {
     File file =
-        new File(String.format(fileName, pathForOutput, LocalDate.now().getMonth().name()));
+        new File(String.format(FILE_NAME, pathForOutput, LocalDate.now().getMonth().name()));
     try (FileOutputStream fos = new FileOutputStream(file)) {
-      Workbook workbook = excelWorkbookGenerator.generateReportWorkbook(reportType, collectWorkloadData());
+      Workbook workbook = excelWorkbookGenerator.generateReportWorkbook(reportType,
+          collectWorkloadData());
       workbook.write(fos);
+      workbook.close();
     } catch (IOException e) {
       throw new ReportNotFoundException(e.getMessage());
     }
@@ -77,7 +87,8 @@ public class WorkloadJob {
   @Transactional(readOnly = true)
   public Map<DepartmentResponse, Map<UserResponse, Set<ProjectResponse>>> collectWorkloadData() {
     List<Department> departments = departmentRepository.findAll();
-    Map<DepartmentResponse, Map<UserResponse, Set<ProjectResponse>>> departmentsWorkload = new HashMap<>();
+    Map<DepartmentResponse, Map<UserResponse, Set<ProjectResponse>>> departmentsWorkload =
+        new HashMap<>();
     departments.forEach(
         department -> {
           Map<UserResponse, Set<ProjectResponse>> hashMap = new HashMap<>();
@@ -86,9 +97,8 @@ public class WorkloadJob {
                   hashMap.put(
                       userConverter.toUserResponse(employee),
                       projectConverter.projectSetToProjectRespSet(new HashSet<>(projectRepository
-                          .findValidProjectsByEmployeesId(
-                              employee.getId(), LocalDate.now().minusMonths(1), LocalDate.now())))));
-
+                          .findActiveProjectForCurrentMonthByEmployeeId(employee.getId(),
+                              dateMonthAgo, dateCurrentMonth)))));
           departmentsWorkload.put(departmentConverter.toDepartmentResponse(department), hashMap);
         });
 
