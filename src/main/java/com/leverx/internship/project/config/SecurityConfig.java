278ee1d8e3ffb33a2d3c339fc5f6ada9aca93e2e
funkylop <grommog6@mail.ru>
2022-02-17 15:26:20 +0300

    m3 - Job_&_Report_to_collect_statistics - Report_to_collect_statistics:
    Reformatted code according to google-formatter.

--- src/main/java/com/leverx/internship/project/config/SecurityConfig.java
+++ src/main/java/com/leverx/internship/project/config/SecurityConfig.java
@@ -18,11 +18,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final JwtConfigurer jwtConfigurer;
-  private static final String[] ALL_USERS_PERMISSION = new String[] {"/api/users/**", "/api/projects/{id}", "/api/departments/{id}"};
-  private static final String[] LOGOUT_ENDPOINT = new String[] {"/api/auth/logout"};
+  private static final String[] ALL_USERS_PERMISSION = new String[]{"/api/users/**",
+      "/api/projects/{id}", "/api/departments/{id}"};
+  private static final String[] LOGOUT_ENDPOINT = new String[]{"/api/auth/logout"};
   private static final String[] PERMIT_ALL =
-      new String[] {"/api/auth/login", "/api/swagger-ui/**", "/api/v3/api-docs"};
-  private static final String[] WRITE_DELETE_USER_PERMISSION = new String[] {"/api/departments/**", "/api/projects/**", "/api/report/**"};
+      new String[]{"/api/auth/login", "/api/swagger-ui/**", "/api/v3/api-docs"};
+  private static final String[] WRITE_DELETE_USER_PERMISSION = new String[]{"/api/departments/**",
+      "/api/projects/**", "/api/report/**"};
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
