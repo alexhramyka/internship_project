ff92d387081f824b70f3c852f8b9b9d5deca7682
funkylop <grommog6@mail.ru>
2022-02-17 14:51:55 +0300

    m3 - Job_&_Report_to_collect_statistics - Report_to_collect_statistics:
    Added param: authHeader, changes according to google-formatter.

--- src/main/java/com/leverx/internship/project/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/leverx/internship/project/security/jwt/JwtTokenProvider.java
@@ -2,7 +2,15 @@ package com.leverx.internship.project.security.jwt;
 
 import com.leverx.internship.project.exception.JwtAuthenticationException;
 import com.leverx.internship.project.security.model.Role;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Base64;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,78 +22,80 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
-import java.util.Date;
 
 @Component
 @PropertySource("classpath:application.properties")
 public class JwtTokenProvider {
 
-    @Value("${jwt.secret}")
-    private String secret;
+  @Value("${jwt.secret}")
+  private String secret;
 
-    @Value("${jwt.expired}")
-    private long validityInMilliseconds;
+  @Value("${jwt.expired}")
+  private long validityInMilliseconds;
 
-    private final UserDetailsService userDetailsService;
+  @Value("${auth.header}")
+  private String authHeader;
 
-    @Autowired
-    public JwtTokenProvider(
-        @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+  private final UserDetailsService userDetailsService;
+
+  @Autowired
+  public JwtTokenProvider(
+      @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
+
+  @PostConstruct
+  protected void init() {
+    secret = Base64.getEncoder().encodeToString(secret.getBytes());
+  }
+
+  public String createToken(String username, Role role) {
+    Claims claims = Jwts.claims().setSubject(username);
+    claims.put("role", getRole(role));
+    Date now = new Date();
+    Date validity = new Date(now.getTime() + validityInMilliseconds);
+    return Jwts.builder()
+        .setClaims(claims)
+        .setIssuedAt(now)
+        .setExpiration(validity)
+        .signWith(SignatureAlgorithm.HS256, secret)
+        .compact();
+  }
+
+  public Authentication getAuthentication(String token) {
+    UserDetails userDetails = userDetailsService.loadUserByUsername(getUsername(token));
+    return new UsernamePasswordAuthenticationToken(userDetails, "",
+        userDetails.getAuthorities());
+  }
+
+  public String getUsername(String token) {
+    return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+  }
+
+  public String resolveToken(HttpServletRequest req) {
+    String bearerToken = req.getHeader(authHeader);
+    if (bearerToken != null) {
+      return bearerToken.substring(7);
+    } else {
+      return null;
     }
+  }
 
-    @PostConstruct
-    protected void init() {
-        secret = Base64.getEncoder().encodeToString(secret.getBytes());
+  public boolean validateToken(String token) throws JwtException {
+    try {
+      Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+      return !claims.getBody().getExpiration().before(new Date());
+    } catch (JwtException | IllegalArgumentException e) {
+      throw new JwtAuthenticationException("JWT token is expired or invalid");
     }
+  }
 
-    public String createToken(String username, Role role) {
-        Claims claims = Jwts.claims().setSubject(username);
-        claims.put("role", getRole(role));
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
-    }
+  private String getRole(Role role) {
+    return role.toString();
+  }
 
-    public Authentication getAuthentication(String token) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(getUsername(token));
-        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-    }
-
-    public String getUsername(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
-    }
-
-    public String resolveToken(HttpServletRequest req) {
-        String bearerToken = req.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        } else return null;
-    }
-
-    public boolean validateToken(String token) throws JwtException {
-        try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
-            return !claims.getBody().getExpiration().before(new Date());
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new JwtAuthenticationException("JWT token is expired or invalid");
-        }
-    }
-
-    private String getRole(Role role) {
-        return role.toString();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
-    }
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
 }
